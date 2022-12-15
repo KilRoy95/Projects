58fe9eeee93c78a914076b7297e1bbf0a48961a7
KilRoy95 <75723624+KilRoy95@users.noreply.github.com>
2022-12-15 15:17:41 +0100

    1.0

--- Work In Progress/Rock Paper Scissors/main.js
+++ Work In Progress/Rock Paper Scissors/main.js
@@ -1,36 +1,45 @@
     let playerScore = 0;
     let compScore = 0;
+    let winState = false;
+    let playerSB = 0;
+    let compSB = 0;
+
     document.getElementById("PlayerScore").innerHTML = "Player: " + playerScore;
     document.getElementById("CompScore").innerHTML = "Computer: " + compScore;
 
+    //Function for handeling game logics (pre-Win function)
     function PointHandler(e){
+        if (winState == false){
 
-        const x = Math.floor(Math.random() * 3) +1;
-        console.log(e + "\n" + x +"\n");
-
-        if (e == 1 && x == 3 || e == 2 && x == 1 || e == 3 && x == 2){
-            playerScore++;
-        }else (compScore++);
-
-        document.getElementById("PlayerScore").innerHTML = "Player: " + playerScore;
-        document.getElementById("CompScore").innerHTML = "Computer: " + compScore;
-
-        if (playerScore >= 3){
-            document.getElementById("Score").innerHTML = "You Win!"
-
-            document.getElementById("playAgainBtn").style.visibility = "visible";
-        }
-
-        if (compScore >= 3){
-            document.getElementById("Score").innerHTML = "You Lose!"
-
-            document.getElementById("playAgainBtn").style.visibility = "visible";
+            const x = Math.floor(Math.random() * 3) +1;
+            console.log(e + "\n" + x +"\n");
+    
+            if (e == 1 && x == 3 || e == 2 && x == 1 || e == 3 && x == 2){
+                playerScore++;
+            }else (compScore++);
+    
+            document.getElementById("PlayerScore").innerHTML = "Player: " + playerScore;
+            document.getElementById("CompScore").innerHTML = "Computer: " + compScore;
+    
+            if (playerScore >= 3){
+                winState = true;
+                playerSB++;
+                document.getElementById("Score").innerHTML = "You Win!"
+                document.getElementById("playAgainBtn").style.visibility = "visible";
+            }else if (compScore >= 3){
+                winState = true;
+                compSB++;
+                document.getElementById("Score").innerHTML = "You Lose!"
+                document.getElementById("playAgainBtn").style.visibility = "visible";
+            }
         }
     }
     
+    //Function for handeling past-win condition (reset-function)
     function Reset(){
         playerScore = 0;
         compScore = 0;
+        winState = false;
 
         document.getElementById("Score").innerHTML = "Score";
 
@@ -38,4 +47,6 @@
         document.getElementById("CompScore").innerHTML = "Computer: " + compScore;
 
         document.getElementById("playAgainBtn").style.visibility = "hidden";
+
+        document.getElementById("Score").innerHTML = playerSB + " - Score - " + compSB;
     }
